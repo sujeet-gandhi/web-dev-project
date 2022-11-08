df9f5dcdfb69439d5256419f5ee232d97e700423
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-11-07 21:40:19 -0600

    backend init

---
+++ backend/onestopgo/src/main/java/com/neu/onestopgo/OnestopgoApplication.java
@@ -0,0 +1,13 @@
+package com.neu.onestopgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnestopgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnestopgoApplication.class, args);
+	}
+
+}
---
+++ backend/onestopgo/src/test/java/com/neu/onestopgo/OnestopgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neu.onestopgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnestopgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
