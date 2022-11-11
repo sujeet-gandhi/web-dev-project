0e9a28d95a4f06ad61e1e7cf70901db95ae80ea0
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-11-11 16:56:50 -0600

    user model and db config

---
+++ backend/onestopgo/src/main/java/com/neu/onestopgo/models/User.java
@@ -0,0 +1,58 @@
+package com.neu.onestopgo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String email;
+    private String password;
+
+    private String address;
+    private String contact;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}
