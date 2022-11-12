35df0057a787e163e535cd760acdc773214a6e3e
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-11-12 02:42:48 -0600

    updated style anuj

---
+++ backend/onestopgo/src/main/java/com/neu/onestopgo/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package com.neu.onestopgo.controllers;
+
+import com.neu.onestopgo.models.Category;
+import com.neu.onestopgo.models.Store;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController("/api/v1")
+public class HomeController {
+
+    @GetMapping(path = "/home")
+    public ResponseEntity getHome() {
+        Store walmart = new Store()
+                .setId(1)
+                .setName("Walmart")
+                .setImageUrl("https://s3.amazonaws.com/www-inside-design/uploads/2018/04/walmart-square.jpg");
+
+        Category grocery = new Category(1, "Grocery", "Get Groceries and More");
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("userId", 1);
+        response.put("stores", List.of(walmart, walmart, walmart, walmart, walmart, walmart));
+        response.put("categories", List.of(grocery, grocery, grocery, grocery, grocery, grocery));
+
+        return ResponseEntity.ok(response);
+    }
+
+}
