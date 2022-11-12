920fe236157a27888f0242820fd2f07aad267081
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-11-11 20:11:39 -0600

    api/v1 create store

--- backend/onestopgo/src/main/java/com/neu/onestopgo/controller/StoreController.java
+++ backend/onestopgo/src/main/java/com/neu/onestopgo/controller/StoreController.java
@@ -5,9 +5,11 @@ import com.neu.onestopgo.services.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/")
+@RestController
+@RequestMapping("/api/v1")
 public class StoreController {
     @Autowired
     private StoreService storeService;
