9ce3810ed5ebbac9245befb6440590b5c84bea44
anizmo <potdar.anuj@gmail.com>
2022-11-14 14:18:23 -0500

    Many-to-Many Stores-Orders and Search API Outline
    - Addressing PR Comments
    - ProductRepository : Changed key from Integer to UUID

--- backend/onestopgo/src/main/java/com/neu/onestopgo/repositories/ProductRepository.java
+++ backend/onestopgo/src/main/java/com/neu/onestopgo/repositories/ProductRepository.java
@@ -3,4 +3,6 @@ package com.neu.onestopgo.repositories;
 import com.neu.onestopgo.models.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {}
+import java.util.UUID;
+
+public interface ProductRepository extends CrudRepository<Product, UUID> {}
