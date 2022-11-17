0640b850596a6ad94e416f4ffe964a8c26961bd0
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-11-17 11:15:30 -0600

    root, see store details,
    create store Form

--- frontend/onestopgo/src/root-operations/index.js
+++ frontend/onestopgo/src/root-operations/index.js
@@ -12,55 +12,67 @@ const RootOperations = () => {
     // }, []);
 
     return (
-        <div className="row">
-            <div className="col-3 scrolling">
-                {loading && <h3>Loading ...</h3>}
-                {!loading &&
-                    <>
-                        <ul>
-                            {homeData.stores.map((store) => <StoreItemDetails key={store.id} store={store}/>)}
-                        </ul>
-                    </>
-                }
-            </div>
-            <div className="col-9">
-                <div className="form-floating wd-top-margin-form">
-                    <input type="text" className="form-control text-bg-light" id="name" name="name"
-                           value=""/>
-                    <label htmlFor="name">Name</label>
+        <div className="mt-2">
+            <div className="row">
+                <div className="col-3 scrolling">
+                    {loading && <h3>Loading ...</h3>}
+                    {!loading &&
+                        <>
+                            <ul>
+                                {homeData.stores.map((store) => <StoreItemDetails key={store.id} store={store}/>)}
+                            </ul>
+                        </>
+                    }
                 </div>
-                <div className="form-floating mt-2">
-                    <input type="text" className="form-control text-bg-light" id="location" name="location"
-                           value=""/>
-                    <label htmlFor="location">Location</label>
+                <div className="col-9">
+                    <div className="border border-1 ">
+                        <div className="ms-2 me-2 mt-2 mb-2">
+                            <div className="fs-2 text-center">
+                                Create Store
+                            </div>
+                            <div className="form-floating wd-top-margin-form">
+                                <input type="text" className="form-control text-bg-light" id="name" name="name"
+                                       value=""/>
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <div className="form-floating mt-2">
+                                <input type="text" className="form-control text-bg-light" id="location" name="location"
+                                       value=""/>
+                                <label htmlFor="location">Location</label>
+                            </div>
+                            <div className="form-floating mt-2">
+                                <select className="form-control text-bg-light" id="type" name="type">
+                                    <option value="SUPERMARKET">SUPERMARKET</option>
+                                    <option value="GROCERY">GROCERY</option>
+                                    <option value="HARDWARE">HARDWARE</option>
+                                </select>
+                                <label htmlFor="type">Type</label>
+                            </div>
+                            <div className="form-floating mt-2">
+                                <input type="time" className="form-control text-bg-light" id="openingTime"
+                                       name="openingTime"
+                                       value=""/>
+                                <label htmlFor="openingTime">Opening Time</label>
+                            </div>
+                            <div className="form-floating mt-2">
+                                <input type="time" className="form-control text-bg-light" id="closingTime"
+                                       name="closingTime"
+                                       value=""/>
+                                <label htmlFor="closingTime">Closing Time</label>
+                            </div>
+                            <div>
+                                <input type="file" className="form-control text-bg-light" id="storeImage"
+                                       name="storeImage"/>
+                                <label htmlFor="storeImage">Store Image</label>
+                            </div>
+                            <div className="text-center">
+                                <button className="rounded-pill w-50">Create Store</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="form-floating mt-2">
-                    <select className="form-control text-bg-light" id="type" name="type">
-                        <option value="SUPERMARKET">SUPERMARKET</option>
-                        <option value="GROCERY">GROCERY</option>
-                        <option value="HARDWARE">HARDWARE</option>
-                    </select>
-                    <label htmlFor="type">Type</label>
-                </div>
-                <div className="form-floating mt-2">
-                    <input type="time" className="form-control text-bg-light" id="openingTime" name="openingTime"
-                           value=""/>
-                    <label htmlFor="openingTime">Opening Time</label>
-                </div>
-                <div className="form-floating mt-2">
-                    <input type="time" className="form-control text-bg-light" id="closingTime" name="closingTime"
-                           value=""/>
-                    <label htmlFor="closingTime">Closing Time</label>
-                </div>
-                <div>
-                    <input type="file" className="form-control text-bg-light" id="storeImage" name="storeImage"/>
-                    <label htmlFor="storeImage">Store Image</label>
-                </div>
-                <div className="text-center">
-                    <button className="rounded-pill w-50">Create Store</button>
-                </div>
-            </div>
 
+            </div>
         </div>
     );
 }
