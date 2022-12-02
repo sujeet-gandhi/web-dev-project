bfcf700f2a03f04832a2a521ec23dfef0d3d7189
sujeet-gandhi <sujeetgandhi014@gmail.com>
2022-12-02 01:11:12 -0800

    root operation minor changes forms

--- frontend/onestopgo/src/root-operations/index.js
+++ frontend/onestopgo/src/root-operations/index.js
@@ -88,16 +88,16 @@ const RootOperations = () => {
                 </div>
                 <div className="col-9">
                     <div className="border border-1 ">
-                        <div className="ms-2 me-2 mt-2 mb-2">
-                            <div className="fs-2 text-center">
+                        <div className="ms-2 me-2 mt-2 mb-2 ">
+                            <div className="fs-2 text-center ">
                                 Create Store
                             </div>
-                            <div className="form-floating wd-top-margin-form">
+                            <div className="input-field wd-top-margin-form ">
                                 <input type="text" className="form-control text-bg-light" id="name" name="name"
-                                       value={createStoreState.name} onChange={handleDataEntryStore}/>
+                                       value={createStoreState.name}  onChange={handleDataEntryStore}/>
                                 <label htmlFor="name">Name</label>
                             </div>
-                            <div className="form-floating mt-2">
+                            <div className="input-field mt-2">
                                 <input type="text" className="form-control text-bg-light" id="location" name="location"
                                        value={createStoreState.location} onChange={handleDataEntryStore}/>
                                 <label htmlFor="location">Location</label>
@@ -112,17 +112,19 @@ const RootOperations = () => {
                                 </select>
                                 <label htmlFor="type">Type</label>
                             </div>
-                            <div className="form-floating mt-2">
-                                <input type="time" className="form-control text-bg-light" id="openingTime"
-                                       name="openingTime"
-                                       value={createStoreState.openingTime} onChange={handleDataEntryStore}/>
-                                <label htmlFor="openingTime">Opening Time</label>
-                            </div>
-                            <div className="form-floating mt-2">
-                                <input type="time" className="form-control text-bg-light" id="closingTime"
-                                       name="closingTime"
-                                       value={createStoreState.closingTime} onChange={handleDataEntryStore}/>
-                                <label htmlFor="closingTime">Closing Time</label>
+                            <div className="row mt-2">
+                                <div className="col-6 form-floating">
+                                    <input type="time" className="form-control text-bg-light" id="openingTime"
+                                           name="openingTime"
+                                           value={createStoreState.openingTime} onChange={handleDataEntryStore}/>
+                                    <label htmlFor="openingTime">Opening Time</label>
+                                </div>
+                                <div className="col-6 form-floating">
+                                    <input type="time" className="form-control text-bg-light" id="closingTime"
+                                           name="closingTime"
+                                           value={createStoreState.closingTime} onChange={handleDataEntryStore}/>
+                                    <label htmlFor="closingTime">Closing Time</label>
+                                </div>
                             </div>
                             <div>
                                 <input type="file" className="form-control text-bg-light" id="imageUrl"
@@ -142,23 +144,23 @@ const RootOperations = () => {
                             <div className="fs-2 text-center">
                                 Create Store Admin
                             </div>
-                            <div className="form-floating wd-top-margin-form">
+                            <div className="input-field ">
                                 <input type="email" className="form-control text-bg-light" id="email" name="email"
                                        value={createStoreAdminState.email} onChange={handleDataEntryStoreAdmin}/>
                                 <label htmlFor="email">Email</label>
                             </div>
-                            <div className="form-floating mt-2">
+                            <div className="input-field">
                                 <input type="text" className="form-control text-bg-light" id="password" name="password"
                                        value={createStoreAdminState.password} onChange={handleDataEntryStoreAdmin}/>
                                 <label htmlFor="password">Password</label>
                             </div>
-                            <div className="form-floating mt-2">
+                            <div className="input-field">
                                 <input type="text" className="form-control text-bg-light" id="adminAddress"
                                        name="adminAddress"
                                        value={createStoreAdminState.address} onChange={handleDataEntryStoreAdmin}/>
                                 <label htmlFor="adminAddress">Address</label>
                             </div>
-                            <div className="form-floating mt-2">
+                            <div className="input-field mt-2">
                                 <input type="text" className="form-control text-bg-light" id="adminContact"
                                        name="adminContact"
                                        value={createStoreAdminState.contact} onChange={handleDataEntryStoreAdmin}/>
@@ -195,12 +197,12 @@ const RootOperations = () => {
                             <div className="fs-2 text-center">
                                 Create Category
                             </div>
-                            <div className="form-floating wd-top-margin-form">
+                            <div className="input-field wd-top-margin-form">
                                 <input type="text" className="form-control text-bg-light" id="categoryName" name="name"
                                        value={createCategoryState.name} onChange={handleDataEntryCreateCategory}/>
                                 <label htmlFor="categoryName">Name</label>
                             </div>
-                            <div className="form-floating wd-top-margin-form">
+                            <div className="input-field wd-top-margin-form">
                                 <input type="text" className="form-control text-bg-light" id="categoryDes" name="description"
                                        value={createCategoryState.description} onChange={handleDataEntryCreateCategory}/>
                                 <label htmlFor="categoryDes">Description</label>
