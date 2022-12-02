8df1860d6368202d7ddefa0d784fecc2200b0f30
sujeet-gandhi <sujeetgandhi014@gmail.com>
2022-12-01 22:56:44 -0800

    text over image home screen

--- frontend/onestopgo/src/categories/category-item.js
+++ frontend/onestopgo/src/categories/category-item.js
@@ -5,16 +5,18 @@ const ONESTOPGO_API = process.env.REACT_APP_ONESTOPGO_API_BASE;
 
 const CategoryItem = ({category: category}) => {
     return (
-            <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
-                <Link to={'/category/'+category.id} className="wd-remove-text-decoration">
-                    <div className="card teal wd-remove-text-decoration">
-                        <div className="card-content center white-text wd-category-text">
-                            <span className="card-title fw-bold">{category.name}</span>
-                            <span>{category.description}</span>
-                        </div>
+        <div className="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6">
+            <Link to={'/category/'+category.id} className="wd-remove-text-decoration">
+                <div className="card">
+                    <div className="card-image">
+                        <img className="img-fluid" src="/images/stock-grocery.jpeg"/>
+                        <span className="card-title fw-bold mb-5">{category.name} </span>
+                        <span className="card-title">{category.description}</span>
                     </div>
-                </Link>
-            </div>
+                </div>
+            </Link>
+        </div>
+
     );
 };
 export default CategoryItem;
