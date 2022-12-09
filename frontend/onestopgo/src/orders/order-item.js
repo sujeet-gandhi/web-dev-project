06589ffde39ccfd8822e3a4bc77ad57ef9564a49
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-12-08 20:05:01 -0500

    total, user mail shown on order page

--- frontend/onestopgo/src/orders/order-item.js
+++ frontend/onestopgo/src/orders/order-item.js
@@ -2,6 +2,7 @@ import CartItem from "../cart/cart-item";
 import React from "react";
 import {cancelOrderThunk, deliverOrderThunk} from "./orders-thunk";
 import {useDispatch} from "react-redux";
+import {Link} from "react-router-dom";
 
 export const OrderItem = ({order, userType}) => {
 
@@ -14,7 +15,7 @@ export const OrderItem = ({order, userType}) => {
     const handleDeliverOrder = () => {
         dispatch(deliverOrderThunk(order));
     }
-    
+
     const uniqueStoreNames = new Set();
     order.items.forEach((each) => uniqueStoreNames.add(each.storeName))
 
@@ -36,18 +37,28 @@ export const OrderItem = ({order, userType}) => {
                     <span className="card-title fw-bolder">Order #{order.id}</span>
                     {order.orderState === 'PLACED' &&
                         <a onClick={handleCancelOrder} href="#"
-                                                         className="secondary-content white-text right"><i className="material-icons">cancel</i>
+                           className="secondary-content white-text right"><i className="material-icons">cancel</i>
                         </a>
                     }
 
                     <p>Details </p>
                     <span className={'fw-bolder card-title'}>Order is {order.orderState.toString().toLowerCase()}</span>
                     {
-                        ((userType === "ROOT" || userType === "STOREADMIN") && order.orderState === "PLACED") &&
-                        <div className="fw-bolder card-title">
-                            <button className="btn btn-secondary" onClick={handleDeliverOrder}>Mark as Delivered</button>
-                        </div>
+                        (userType === "ROOT" || userType === "STOREADMIN") &&
+                        <>
+                            {
+                                (order.orderState === "PLACED") &&
+                                <div className="fw-bolder card-title">
+                                    <button className="btn btn-secondary" onClick={handleDeliverOrder}>Mark as Delivered
+                                    </button>
+                                </div>
+                            }
+                            <div className="fw-bold fs-5 card-title">
+                                Customer Id : <Link to={'/profile/' + order.userId} className="text-white">{order.userEmail}</Link>
+                            </div>
+                        </>
                     }
+                    <h4 className={'right'}>Total: ${order.orderTotal}</h4>
                 </div>
                 <div className={'card-action'}>
                     <ul className={'collection'}>
